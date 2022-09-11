590e1e9e261c9d14164dad11d9b7b79e49d96d94
Gwen <e1307685@student.tuwien.ac.at>
2022-09-11 17:35:19 +0200

    Add checking for empty files
    We check if files exists but not if they are empty. Mothur throws an error for empty files, so we added an additional check.

--- src/mothur_process.py
+++ src/mothur_process.py
@@ -34,8 +34,25 @@ def get_mothur() -> Mothur:
         return None
 
 
-def run_mothur(input_dir: str, output_dir: str):
+def check_file(input_dir: str, file: str) -> bool:
+    """
+    Checks if file exists and if file is not empty. Returns True if file does exist or is empty.
+    :return: True if file exists and is not empty
+    """
+    if os.path.isfile(f"{input_dir}/{file}"):
+        print(f"File {file} exists", end=" ")
+        if os.stat(f"{input_dir}/{file}").st_size != 0:
+            print("and is not empty.")
+            return True
+        print("but is empty")
+        os.remove(f"{input_dir}/{file}")
+        return False
+    else:
+        print(f"{file} does not exist")
+        return False
 
+
+def run_mothur(input_dir: str, output_dir: str, rerun: bool = False) -> None:
     input_dir = f"{ROOT}/{input_dir}"
     output_dir = f"{ROOT}/{output_dir}"
 
@@ -48,30 +65,34 @@ def run_mothur(input_dir: str, output_dir: str):
     m.set.logfile(name=config["logfile_name"], append="T")
     m.set.dir(input=input_dir)  # set folder where mothur is looking for input files
 
-    if not os.path.isfile(f"{input_dir}/{prefix}.files"):
+    if rerun:
+        print("Rerunning mothur on already existing files.")
+
+    if not check_file(input_dir,f"{prefix}.files") or rerun:
         m.make.file(inputdir=input_dir, type=config["data_type"], prefix=prefix)
 
-    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.fasta"):
+    if not check_file(input_dir,f"{prefix}.trim.contigs.fasta") or rerun:
         m.make.contigs(file=f"{prefix}.files", processors=processors, inputdir=input_dir)
 
-    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.fasta"):
+    if not check_file(input_dir,f"{prefix}.trim.contigs.good.fasta") or rerun:
         m.screen.seqs(fasta=f"{prefix}.trim.contigs.fasta", contigsreport=f"{prefix}.contigs.report",
-                      group=f"{prefix}.contigs.groups", maxambig=0, maxhomop=6, minlength=200, maxlength=1000, mismatches=0,
+                      group=f"{prefix}.contigs.groups", maxambig=0, maxhomop=6, minlength=200, maxlength=1000,
+                      mismatches=0,
                       processors=processors, inputdir=input_dir)
 
-    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.fasta"):
+    if not check_file(input_dir,f"{prefix}.trim.contigs.good.trim.fasta") or rerun:
         m.trim.seqs(fasta=f"{prefix}.trim.contigs.good.fasta", processors=processors, qaverage=25, inputdir=input_dir)
 
-    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.unique.fasta"):
+    if not check_file(input_dir,f"{prefix}.trim.contigs.good.trim.unique.fasta") or rerun:
         m.unique.seqs(fasta=f"{prefix}.trim.contigs.good.trim.fasta", inputdir=input_dir)
 
-    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.count_table"):
+    if not check_file(input_dir,f"{prefix}.trim.contigs.good.trim.count_table") or rerun:
         m.count.seqs(name=f"{prefix}.trim.contigs.good.trim.names", group=f"{prefix}.contigs.good.groups",
-                 inputdir=input_dir)
+                     inputdir=input_dir)
 
     tax_dir = f"{ROOT}/src/mothur_files"
     # assign their sequences to the taxonomy outline of rdp file (version 6)
-    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.unique.trainset6_032010.wang.taxonomy"):
+    if not check_file(input_dir,f"{prefix}.trim.contigs.good.trim.unique.trainset6_032010.wang.taxonomy") or rerun:
         m.classify.seqs(fasta=f"{prefix}.trim.contigs.good.trim.unique.fasta",
                         count=f"{prefix}.trim.contigs.good.trim.count_table",
                         template=f"{tax_dir}/trainset6_032010.fa",
@@ -80,7 +101,7 @@ def run_mothur(input_dir: str, output_dir: str):
                         processors=processors)
 
     # assign their sequences to the taxonomy outline of rdp file (version 18)
-    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.unique.rdp.wang.taxonomy"):
+    if not check_file(input_dir,f"{prefix}.trim.contigs.good.trim.unique.rdp.wang.taxonomy") or rerun:
         m.classify.seqs(fasta=f"{prefix}.trim.contigs.good.trim.unique.fasta",
                         count=f"{prefix}.trim.contigs.good.trim.count_table",
                         template=f"{tax_dir}/trainset18_062020.rdp.fasta",
@@ -88,17 +109,17 @@ def run_mothur(input_dir: str, output_dir: str):
                         output="simple", inputdir=input_dir,
                         processors=processors)
 
-    if os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.unique.trainset6_032010.wang.tax.summary"):
+    if check_file(input_dir,f"{prefix}.trim.contigs.good.trim.unique.trainset6_032010.wang.tax.summary"):
         m.rename.file(input=f"{prefix}.trim.contigs.good.trim.unique.trainset6_032010.wang.tax.summary",
-                  new="final.rdp6.summary",
-                  inputdir=input_dir, outputdir=output_dir)
+                      new="final.rdp6.summary",
+                      inputdir=input_dir, outputdir=output_dir)
 
-    if os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.unique.rdp.wang.tax.summary"):
+    if check_file(input_dir,f"{prefix}.trim.contigs.good.trim.unique.rdp.wang.tax.summary"):
         m.rename.file(input=f"{prefix}.trim.contigs.good.trim.unique.rdp.wang.tax.summary", new="final.rdp18.summary",
-                  inputdir=input_dir, outputdir=output_dir)
+                      inputdir=input_dir, outputdir=output_dir)
 
 
 if __name__ == "__main__":
     ROOT = ".."
-    run_mothur(input_dir="data/vagina_momspi/visit1",
-               output_dir="mothur_output/vagina_momspi/visit1")
+    run_mothur(input_dir=f"data/buccal_mucosa_momspi/visit1",
+               output_dir="mothur_output/buccal_mucosa_momspi/visit1", rerun=True)
