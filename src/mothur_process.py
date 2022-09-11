bfdb98ed3600972ccc7394afc9ecca2cb1d56dda
Gwen <e1307685@student.tuwien.ac.at>
2022-09-11 12:10:50 +0200

    Add processor option to mothur config

--- src/mothur_process.py
+++ src/mothur_process.py
@@ -35,57 +35,70 @@ def get_mothur() -> Mothur:
 
 
 def run_mothur(input_dir: str, output_dir: str):
-    with open(f"{ROOT}/src/mothur_files/mothur_config.json", "r") as jsonfile:
-        config = json.load(jsonfile)
 
     input_dir = f"{ROOT}/{input_dir}"
     output_dir = f"{ROOT}/{output_dir}"
 
-    PREFIX = config["file_prefix"]
+    with open(f"{ROOT}/src/mothur_files/mothur_config.json", "r") as jsonfile:
+        config = json.load(jsonfile)
+    prefix = config["file_prefix"]
+    processors = config["processors"]
 
     m = get_mothur()
-    # m.set.logfile(name=config["logfile_name"], append="T")
-
-    m.set.logfile(name="silent")
-    # TODO flexible input folder
+    m.set.logfile(name=config["logfile_name"], append="T")
     m.set.dir(input=input_dir)  # set folder where mothur is looking for input files
-    m.make.file(inputdir=input_dir, type=config["data_type"], prefix=PREFIX)
-    m.make.contigs(file=f"{PREFIX}.files", processors=8, inputdir=input_dir)
-    m.screen.seqs(fasta="stability.trim.contigs.fasta", contigsreport="stability.contigs.report",
-                  group="stability.contigs.groups", maxambig=0, maxhomop=6, minlength=200, maxlength=1000, mismatches=0,
-                  processors=56, inputdir=input_dir)
 
-    m.trim.seqs(fasta="stability.trim.contigs.good.fasta", processors=56, qaverage=25, inputdir=input_dir)
-    m.unique.seqs(fasta="stability.trim.contigs.good.trim.fasta", inputdir=input_dir)
+    if not os.path.isfile(f"{input_dir}/{prefix}.files"):
+        m.make.file(inputdir=input_dir, type=config["data_type"], prefix=prefix)
 
-    m.count.seqs(name="stability.trim.contigs.good.trim.names", group="stability.contigs.good.groups",
+    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.fasta"):
+        m.make.contigs(file=f"{prefix}.files", processors=processors, inputdir=input_dir)
+
+    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.fasta"):
+        m.screen.seqs(fasta=f"{prefix}.trim.contigs.fasta", contigsreport=f"{prefix}.contigs.report",
+                      group=f"{prefix}.contigs.groups", maxambig=0, maxhomop=6, minlength=200, maxlength=1000, mismatches=0,
+                      processors=processors, inputdir=input_dir)
+
+    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.fasta"):
+        m.trim.seqs(fasta=f"{prefix}.trim.contigs.good.fasta", processors=processors, qaverage=25, inputdir=input_dir)
+
+    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.unique.fasta"):
+        m.unique.seqs(fasta=f"{prefix}.trim.contigs.good.trim.fasta", inputdir=input_dir)
+
+    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.count_table"):
+        m.count.seqs(name=f"{prefix}.trim.contigs.good.trim.names", group=f"{prefix}.contigs.good.groups",
                  inputdir=input_dir)
 
-    TAX_DIR = f"{ROOT}/src/mothur_files"
+    tax_dir = f"{ROOT}/src/mothur_files"
     # assign their sequences to the taxonomy outline of rdp file (version 6)
-    m.classify.seqs(fasta="stability.trim.contigs.good.trim.unique.fasta",
-                    count="stability.trim.contigs.good.trim.count_table",
-                    template=f"{TAX_DIR}/trainset6_032010.fa",
-                    taxonomy=f"{TAX_DIR}/trainset6_032010.tax",
-                    output="simple", inputdir=input_dir,
-                    processors=56)
+    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.unique.trainset6_032010.wang.taxonomy"):
+        m.classify.seqs(fasta=f"{prefix}.trim.contigs.good.trim.unique.fasta",
+                        count=f"{prefix}.trim.contigs.good.trim.count_table",
+                        template=f"{tax_dir}/trainset6_032010.fa",
+                        taxonomy=f"{tax_dir}/trainset6_032010.tax",
+                        output="simple", inputdir=input_dir,
+                        processors=processors)
 
     # assign their sequences to the taxonomy outline of rdp file (version 18)
-    m.classify.seqs(fasta="stability.trim.contigs.good.trim.unique.fasta",
-                    count="stability.trim.contigs.good.trim.count_table",
-                    template=f"{TAX_DIR}/trainset18_062020.rdp.fasta",
-                    taxonomy=f"{TAX_DIR}/trainset18_062020.rdp.tax",
-                    output="simple", inputdir=input_dir,
-                    processors=56)
+    if not os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.unique.rdp.wang.taxonomy"):
+        m.classify.seqs(fasta=f"{prefix}.trim.contigs.good.trim.unique.fasta",
+                        count=f"{prefix}.trim.contigs.good.trim.count_table",
+                        template=f"{tax_dir}/trainset18_062020.rdp.fasta",
+                        taxonomy=f"{tax_dir}/trainset18_062020.rdp.tax",
+                        output="simple", inputdir=input_dir,
+                        processors=processors)
 
-    m.rename.file(input="stability.trim.contigs.good.trim.unique.trainset6_032010.wang.tax.summary",
+    if os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.unique.trainset6_032010.wang.tax.summary"):
+        m.rename.file(input=f"{prefix}.trim.contigs.good.trim.unique.trainset6_032010.wang.tax.summary",
                   new="final.rdp6.summary",
                   inputdir=input_dir, outputdir=output_dir)
-    m.rename.file(input="stability.trim.contigs.good.trim.unique.rdp.wang.tax.summary", new="final.rdp18.summary",
+
+    if os.path.isfile(f"{input_dir}/{prefix}.trim.contigs.good.trim.unique.rdp.wang.tax.summary"):
+        m.rename.file(input=f"{prefix}.trim.contigs.good.trim.unique.rdp.wang.tax.summary", new="final.rdp18.summary",
                   inputdir=input_dir, outputdir=output_dir)
 
 
 if __name__ == "__main__":
     ROOT = ".."
-    run_mothur(input_dir="data/buccal_mucosa_momspi/visit1",
-               output_dir="mothur_output/buccal_mucosa_momspi/visit1")
+    run_mothur(input_dir="data/vagina_momspi/visit1",
+               output_dir="mothur_output/vagina_momspi/visit1")
