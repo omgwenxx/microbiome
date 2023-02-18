99cf5c2b09e2f2577bcbd4eed675f07fe10bd736
Gwen <e1307685@student.tuwien.ac.at>
2023-02-18 18:16:21 +0100

    Fix double visit bug
    Instead of saving visit 1 and visit 2 files with the same IDs, we were saving the same file twice. Explains the 100% accuracy.

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -97,38 +97,56 @@ def unify_files(output_dir:str = "final_data"):
                 current_file = os.path.join(data_dir, body_folder, rdp_folder, input_file)
                 visit = input_file.split("-")[3].split(".")[0]
                 filename = f"otus-{body_folder}-{rdp_folder}-{visit}.pcl"
-                print("Changing taxonomy from file %s" % filename)
                 current_pd = pd.read_csv(current_file, sep='\t')
                 merge = pd.merge(summary, current_pd, how='left', on='subject_id').fillna(0)
                 merge.to_csv(os.path.join(output_dir, body_folder, rdp_folder, filename), sep='\t', index=False)
-                print("Current file has %s features (taxonomy) and %s subjects" % (merge.shape[0], merge.shape[1]))
+                print("Current file (%s) has %s subjects and %s features (taxonomy)" % (filename, merge.shape[1]-1, merge.shape[0]-1))
 
-# %%
+
 def create_double_visits(dir, body_dir, rdp, output_dir = "double_visits"):
     if not os.path.exists(f"{ROOT}/{output_dir}"):
         os.makedirs(f"{ROOT}/{output_dir}")
 
-    # create files with only TP
+    # Get first visits subjects
     file_prefix = f"otus-{body_dir}-{rdp}-"
     filename = f"{file_prefix}visit1.pcl"
+
+    if body_dir == "feces_momspi":
+        filename = f"{file_prefix}visit2.pcl"
     vis1 = pd.read_csv(os.path.join(dir, body_dir, rdp, filename), sep='\t')
     subjects1 = vis1.columns[1:].to_series()
 
-    vis = pd.read_csv(os.path.join(dir, body_dir, rdp, f"{file_prefix}visit2.pcl"),sep='\t')
-    subjects = vis.columns[1:].to_series()
-    TP = subjects1[subjects1.isin(subjects)]
-    double = vis1[TP]
-    double.insert(0, "subject_id", vis1["subject_id"])
-    double.to_csv(os.path.join(output_dir, filename), sep='\t',index=False)
-    double.to_csv(os.path.join(output_dir, f"{file_prefix}visit2.pcl"), sep='\t', index=False)
+    # Get second visits subjects
+    filename2 = f"{file_prefix}visit2.pcl"
+    if body_dir == "feces_momspi":
+        filename2 = f"{file_prefix}visit3.pcl"
+    vis2 = pd.read_csv(os.path.join(dir, body_dir, rdp, filename2),sep='\t')
+    subjects = vis2.columns[1:].to_series()
 
-# %%
+    # get true positives
+    TP = subjects1[subjects1.isin(subjects)]
+    print(f"For {body_dir} there are {len(TP)} subjects in both visits")
+
+    # save only TP from first visit
+    double1 = vis1[TP]
+    double1.insert(0, "subject_id", vis1["subject_id"])
+    double1.to_csv(os.path.join(f"{ROOT}/{output_dir}", filename), sep='\t',index=False)
+
+    # save only TP from second visit
+    double2 = vis2[TP]
+    double2.insert(0, "subject_id", vis2["subject_id"])
+    double2.to_csv(os.path.join(f"{ROOT}/{output_dir}", filename2), sep='\t', index=False)
+
+
 if __name__ == "__main__":
     ROOT = ".."
-    reformat_taxonomy(f"{ROOT}/mothur_output/buccal_mucosa_momspi")
-    unify_files()
+    input_dir = "mismatch_data"
+    #reformat_taxonomy(f"{ROOT}/mothur_output/buccal_mucosa_momspi")
+    #unify_files()
 
+    create_double_visits(f"{ROOT}/{input_dir}", "feces_momspi", "rdp6", "mismatch_double_visits")
+    create_double_visits(f"{ROOT}/{input_dir}", "feces_momspi", "rdp18", "mismatch_double_visits")
     # get double visits
-    for body_dir in os.listdir(f"{ROOT}/final_data"):
-        for rdp in os.listdir(f"{ROOT}/final_data/{body_dir}"):
-            create_double_visits(f"{ROOT}/final_data", body_dir, rdp)
+    #for body_dir in os.listdir(f"{ROOT}/{input_dir}"):
+    #    for rdp in os.listdir(f"{ROOT}/{input_dir}/{body_dir}"):
+    #        create_double_visits(f"{ROOT}/{input_dir}", body_dir, rdp, "mismatch_double_visits")
